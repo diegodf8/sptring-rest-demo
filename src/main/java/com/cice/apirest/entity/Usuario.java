150f88368f36110772df5c35088420d9a8139365
diegodf8 <diegodf8@gmail.com>
2018-06-22 17:19:12 +0200

    Test

--- src/main/java/com/cice/apirest/entity/Usuario.java
+++ src/main/java/com/cice/apirest/entity/Usuario.java
@@ -1,14 +1,17 @@
 package com.cice.apirest.entity;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
+
 import javax.persistence.Table;
 
 @Entity
 @Table(name="usuarios")
 @Data
 public class Usuario {
+    @Id
     private String id;
     private String nombre;
     private String apellidos;
